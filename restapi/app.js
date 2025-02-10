f00599d9742cfc7860043c6d11129dde4af32ddd
Joel Lamsijärvi <joel.lamsijarvi@live.com>
2025-02-10 19:50:20 +0200

    Tehtävä 19 palautus

--- restapi/app.js
+++ restapi/app.js
@@ -17,4 +17,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/example',
+    function(request,response){
+        response.send('I am example');
+        console.log('I am example');
+    }
+);
+
+app.get('/example/:name',
+    function(request,response){
+        response.send('Heippa ' + request.params.name);
+        console.log('Heippa ' + request.params.name);
+    }
+);
+
+app.get('/example/:lastname/:firstname',
+    function(request,response){
+        response.send('Heippa ' + request.params.firstname + ' ' + request.params.lastname);
+        console.log('Heippa ' + request.params.firstname + ' ' + request.params.lastname);
+    }
+);
+
+app.post('/',
+    function(request,response){
+        response.send(request.body);
+        console.log(request.body);
+    }
+);
+
+app.put('/',
+    function(request,response){
+        response.send('This ia a PUT request');
+        console.log('This is a PUT request');
+    }
+);
+
 module.exports = app;
